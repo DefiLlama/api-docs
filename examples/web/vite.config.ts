a21c92a3630ab6eca744e043acba908dfccc32a2
treeoflife2 <sol.analyzoor@gmail.com>
2025-08-11 23:52:16 +0530

    rename file and update text for llms.txt

--- examples/web/vite.config.ts
+++ examples/web/vite.config.ts
@@ -1,9 +1,32 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import path from 'path'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    {
+      name: 'serve-llms-txt',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url === '/llms.txt') {
+            const filePath = path.resolve(__dirname, '../../llms.txt')
+            if (fs.existsSync(filePath)) {
+              res.setHeader('Content-Type', 'text/plain')
+              fs.createReadStream(filePath).pipe(res)
+            } else {
+              res.statusCode = 404
+              res.end('Not Found')
+            }
+          } else {
+            next()
+          }
+        })
+      },
+    },
+  ],
   server: {
     port: 5050,
     open: true,
